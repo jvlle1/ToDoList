3878d706db919e52cadb126e80187bc7d53b722b
Joelle Sagun <S2302721@ndonline.ac.uk>
2025-07-02 10:59:20 +0100

    Added addTask function by js

---
+++ script.js
@@ -0,0 +1,15 @@
+function addTask() {
+    // gets the user input with the class inputTask
+    const input = document.querySelector(".inputTask");
+    // gets the ul list on the html
+    const list = document.querySelector(".taskList");
+
+    // empty (li) to represent the new tasks
+    const li = document.createElement("li")
+    // the value user inputted is assigned to the li
+    li.textContent = input.value;
+    // append the new list item to the task list
+    list.appendChild(li);
+
+    input.value = ""; // clears input value to add a new one
+}
