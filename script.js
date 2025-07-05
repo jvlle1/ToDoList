0a46cd26eac4ab481e825cfc9214c8621d8d55dd
Joelle Sagun <S2302721@ndonline.ac.uk>
2025-07-05 16:42:05 +0100

    delete task function

--- script.js
+++ script.js
@@ -8,8 +8,24 @@ function addTask() {
     const li = document.createElement("li")
     // the value user inputted is assigned to the li
     li.textContent = input.value;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete Task";
+    deleteBtn.className = "deleteTask";
+    // when button is clicked, runs the deleteTask function
+    deleteBtn.addEventListener("click", deleteTask ); 
+
+    // inserts in individual task item
+    li.appendChild(deleteBtn);
     // append the new list item to the task list
     list.appendChild(li);
 
     input.value = ""; // clears input value to add a new one
+}
+
+function deleteTask(event) {
+    // refers to the element (button) that triggered the event.
+    const button = event.target; 
+    const li = button.parentElement;    
+    li.remove();
 }
