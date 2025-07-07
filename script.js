e876849046f883defc28d516df760e00ea0f2506
Joelle Sagun <S2302721@ndonline.ac.uk>
2025-07-07 12:12:06 +0100

    added input validation for inputting a task, removed some redundant comments

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-function addTask() {
-    // gets the user input with the class inputTask
-    const input = document.querySelector(".inputTask");
-    // gets the ul list on the html
-    const list = document.querySelector(".taskList");
+const form = document.getElementById("userForm");
 
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskInput = document.querySelector(".inputTask");
+    const message = document.querySelector(".errorMsg");
+
+    // input validation the input
+    if (taskInput.value.trim() === '' || taskInput.value == null) {
+        message.textContent = "Input required";
+    } else {
+        message.textContent = "";
+        addTask(taskInput.value.trim());
+        taskInput.value = ""; 
+    }
+})
+
+function addTask(taskText) {
+    const list = document.querySelector(".taskList");
     // empty (li) to represent the new tasks
     const li = document.createElement("li")
-    // the value user inputted is assigned to the li
-    li.textContent = input.value;
+    // the value passed is assigned to (li)
+    li.textContent = taskText;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete Task";
     deleteBtn.className = "deleteTask";
-    // when button is clicked, runs the deleteTask function
     deleteBtn.addEventListener("click", deleteTask ); 
 
     const markDoneBtn = document.createElement("button");
@@ -23,10 +36,9 @@ function addTask() {
     // inserts in individual task item
     li.appendChild(deleteBtn);
     li.appendChild(markDoneBtn);
+
     // append the new list item to the task list
     list.appendChild(li);
-
-    input.value = ""; // clears input value to add a new one
 }
 
 function deleteTask(event) {
